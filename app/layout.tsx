39596f8ed418111144085f31fad66b5ea0ef4a7a
harsh661 <plasmaharsh@gmail.com>
2024-05-04 21:03:05 +0530

    change clerk theme to dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const font = Inter({ subsets: ["latin"], weight: ['400', '500', '600', '700'] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
         <body className={font.className}>{children}</body>
       </html>
